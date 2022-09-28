927c711c44d54acd900d9cdaeb3dddf1dfa8a6f6
Robert Gannard <robertgannard@gmail.com>
2022-09-28 16:54:53 -0500

    trigger4

--- frontend/bookCentral/src/app/services/endpoint.service.ts
+++ frontend/bookCentral/src/app/services/endpoint.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class EndpointService {
   // public baseEndpoint = 'http://192.168.4.78:5500/api/';
   //public baseEndpoint = 'http://192.168.4.103:5500/api/';
-  public baseEndpoint = 'https://bookcentral.herokuapp.com/api';
+  public baseEndpoint = 'https://bookcentral.herokuapp.com/api/';
   public baseSocketEndpoint = 'ws://bookcentral.herokuapp.com/'
   constructor() { }
 
